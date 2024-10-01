407855a854b087d10a160da11cde584611b3626c
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2024-10-01 09:50:10 +0200

    update 5.9

--- superia/src/pages/_app.tsx
+++ superia/src/pages/_app.tsx
@@ -1,33 +1,12 @@
 import { AppProps } from 'next/app';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { useRouter } from 'next/router';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const domain = process.env.AUTH0_ISSUER_BASE_URL;
-  const clientId = process.env.AUTH0_CLIENT_ID;
-  const redirectUri = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI || 'http://localhost:3000/auth-callback';
-
-  const router = useRouter();
-
-  if (!domain || !clientId) {
-    throw new Error('Missing Auth0 configuration');
-  }
-
-  const handleRedirectCallback = (appState?: any) => {
-    // Redirect to the page the user was on before authentication or home page
-    router.push(appState?.returnTo || '/');
-  };
-  console.log("Auth0Provider is active");
-
   return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
-      onRedirectCallback={handleRedirectCallback}
-    >
+    <UserProvider>
       <Component {...pageProps} />
-    </Auth0Provider>
+    </UserProvider>
   );
 }
 
